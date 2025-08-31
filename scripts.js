c46bee4b52092dec0e7c6bd50a984e094c738d8f
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-08-31 16:27:59 +0200

    Prompt user details for two tasks

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let title1 = prompt("Enter task 1 title:");
+console.log(title1);
+
+let description1 = prompt("Enter task 1 description:");
+console.log(description1);
+
+let status1 = prompt("Enter task 1 status:");
+console.log(status1);
+
+let title2 = prompt("Enter task 2 title:");
+console.log(title2);
+
+let description2 = prompt("Enter task 2 description:");
+console.log(description2);
+
+let status2 = prompt("Enter task 2 title:");
+console.log(status2);
