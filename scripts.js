a3d03075ba4cdc0e692cb74e0bc8320d76de067d
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-08-31 17:01:06 +0200

    replace script tag

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
 let title1 = prompt("Enter task 1 title:");
-console.log(title1);
 
 let description1 = prompt("Enter task 1 description:");
-console.log(description1);
 
 let status1 = prompt("Enter task 1 status:");
-console.log(status1);
+console.log(.status1toLowerCase());
 
 let title2 = prompt("Enter task 2 title:");
-console.log(title2);
 
 let description2 = prompt("Enter task 2 description:");
-console.log(description2);
 
 let status2 = prompt("Enter task 2 title:");
-console.log(status2);
+console.log(.status1toLowerCase());
