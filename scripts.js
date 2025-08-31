d940471141db78b2a50ca8e7359b35f6b0941e41
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-08-31 18:14:40 +0200

    if statement for status 1 and 2

--- scripts.js
+++ scripts.js
@@ -5,6 +5,12 @@ let description1 = prompt("Enter task 1 description:");
 let status1 = prompt("Enter task 1 status:");
 console.log(status1.toLowerCase());
 
+if (status1 === "todo" || "doing" || "done") {
+    console.log(status1);
+} else {
+    alert("Invalid status. Please enter `todo`, `doing`, or `done`.");
+}
+
 let title2 = prompt("Enter task 2 title:");
 
 let description2 = prompt("Enter task 2 description:");
@@ -12,9 +18,10 @@ let description2 = prompt("Enter task 2 description:");
 let status2 = prompt("Enter task 2 status:");
 console.log(status2.toLowerCase());
 
-
-if (status1 && status2 === "todo" || "doing" || "done") {
-    console.log(status1 && status2);
+if (status1 === "todo" || "doing" || "done") {
+    console.log(status2);
 } else {
     alert("Invalid status. Please enter `todo`, `doing`, or `done`.");
 }
+
+
