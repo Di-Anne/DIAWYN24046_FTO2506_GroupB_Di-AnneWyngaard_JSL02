75801d28f3e82165435aff626c582629b99c9af3
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-09-04 11:11:12 +0200

    Create seperate if statements for not done and done

--- scripts.js
+++ scripts.js
@@ -4,41 +4,45 @@ let titleTask1 = prompt("Enter task 1 title:");
 
 let descriptionTask1 = prompt("Enter task 1 description:");
 
-let statusTask1 = prompt("Enter task 1 status:").toLowerCase(); //convert the status input to lowercase
+let statusTask1 = prompt("Enter task 1 status (todo, doing, done):").toLowerCase(); //convert the status input to lowercase
 
-// check if user status input is valid, if not return user to prompting
+// check if user status input is valid, if not return user to prompt
 
 while (statusTask1 !== "todo" && statusTask1 !== "doing" && statusTask1 !== "done") {
-  TaskStatus1 = prompt("Enter Task 1 Status: `todo`, `doing` or `done`").toLowerCase();
+  alert(`Invalid status. Please enter "todo", "doing" or "done"`);
+  statusTask1 = prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
 }
 
 // Check if status is done or not with unique message in console
 
 if (statusTask1 !== "done") {
     console.log("No tasks completed, let's get to work!");
-} else {
-    console.log("Title: Make corrections to JSL01, status: done");
+} 
+
+if (statusTask1 === "done") {
+    console.log(titleTask1 + ": " + "Make corrections to JSL01, " + "status: " + statusTask1);
 }
 
-// Prompt users for task 2
+// Repeat everything for Task 2
 
 let titleTask2 = prompt("Enter task 2 title:");
 
 let descriptionTask2 = prompt("Enter task 2 description:");
 
-let statusTask2 = prompt("Enter task 2 status:").toLowerCase(); //convert the status input to lowercase
+let statusTask2 = prompt("Enter task 2 status (todo, doing, done):").toLowerCase(); //convert the status input to lowercase
 
-// check if user status input is valid, if not return user to prompting
+// check if user status input is valid, if not return user to prompt
 
 while (statusTask2 !== "todo" && statusTask2 !== "doing" && statusTask2 !== "done") {
-  TaskStatus1 = prompt("Enter Task 2 Status: `todo`, `doing` or `done`").toLowerCase();
+    alert(`Invalid status. Please enter "todo", "doing" or "done"`);
+    statusTask2 = prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
 }
-console.log(TaskTitle2, ": ", TaskStatus2);
 
 // Check if status is done or not with unique message in console
 
 if (statusTask2 !== "done") {
     console.log("No tasks completed, let's get to work!");
-} else {
-    console.log("Title: Make corrections to JSL02, status: done");
+
+if (statusTask2 === "done") {
+    console.log(titleTask2 + ": " + "Make corrections to JSL02, " + "status: " + statusTask2);
 }
