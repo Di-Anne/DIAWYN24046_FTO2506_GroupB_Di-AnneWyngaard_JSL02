b9e97d45b199039b788fdc63d3fa8a4cf7ccf497
Di-Anne Wyngaard <dianneleighwyngaard@gmail.com>
2025-09-12 21:10:55 +0200

    Edit loop to taskStatus condition only

--- scripts.js
+++ scripts.js
@@ -6,28 +6,30 @@ let task1Description = prompt("Enter task 1 description:");
 let task1Status = prompt("Enter task 1 status (todo, doing, done):").toLowerCase(); 
 
 // Check if user status input is valid, if not return user to prompt
-while (task1title !== "todo" && task1Description !== "doing" && task1Status !== "done") {
+while (task1Status !== "todo" && task1Status !== "doing" && task1Status !== "done") {
   alert("Invalid status. Please enter 'todo', 'doing' or 'done'");
   task1Status = prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
 }
 
-// Check if task 1 is done and log it
-if (task1Status === "done") {
-    console.log(task1title + ": " + "Status: " + task1Status);
-}
 
 // Repeat everything for Task 2
 let task2Title = prompt("Enter task 2 title:");
 let task2Description = prompt("Enter task 2 description:");
 let task2Status = prompt("Enter task 2 status (todo, doing, done):").toLowerCase(); 
 
-while (task2Title !== "todo" && task2Description !== "doing" && task2Status !== "done") {
-    alert(`Invalid status. Please enter "todo", "doing" or "done"`);
+while (
+    task2Status !== "todo" && task2Status !== "doing" && task2Status !== "done") {
+    alert("Invalid status. Please enter 'todo', 'doing' or 'done'");
     task2Status = prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
 }
 
+// Check if task 1 and 2 is done and log it
+if (task1Status === "done") {
+    console.log(`Title: ${task1Title}. Status: ${task1Status}.`);
+}
+
 if (task2Status === "done") {
-    console.log(task2Title + ": " + "Status: " + task2Status);
+    console.log(`Title: ${task2Title}. Status: ${task2Status}.`);
 }
 
 // If neither task 1 nor 2 is done give encouring message
